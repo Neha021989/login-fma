d15c94a3f5d9465aac5ce6e804824015a86c6962
Neha Choudhary <Neha_Choudhary@epam.com>
2021-05-24 14:48:27 +0530

    Minor change

--- src/main/java/com/example/demo/resource/LoginResource.java
+++ src/main/java/com/example/demo/resource/LoginResource.java
@@ -21,39 +21,41 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class LoginResource {
-	
+
 	public final LoginService loginService;
-	
+
 	@Autowired
 	public LoginResource(LoginService loginService) {
 		this.loginService = loginService;
 	}
 
+	@GetMapping("/login")
+	public Mono<String> getHello() {
+		return Mono.just("hello world");
+	}
+
 	@GetMapping("/login/{username}")
-	public Mono<LoginDTO> getLoginDetails(@PathVariable String username){
-		return Mono.just(username).flatMap(u->loginService.getLoginDetails(u))
-				.map(LoginMapper::toApi);
+	public Mono<LoginDTO> getLoginDetails(@PathVariable String username) {
+		return Mono.just(username).flatMap(u -> loginService.getLoginDetails(u)).map(LoginMapper::toApi);
 	}
-	
+
 	@PostMapping("/login")
-	public Mono<ResponseEntity<LoginStatus>> verifyLogin(@RequestBody LoginDTO loginDTO){
-		return Mono.just(LoginMapper.toClient(loginDTO)).flatMap(u->loginService.getLoginDetails(u.getUsername()))
-				.filter(t-> null!= t ?t.getPassword().equals(loginDTO.getPassword()): null)
-				.switchIfEmpty(Mono.just(new Login()))
-				.map(t-> checkStatus(t))
-				.map(r-> new ResponseEntity<>(r, HttpStatus.OK));
+	public Mono<ResponseEntity<LoginStatus>> verifyLogin(@RequestBody LoginDTO loginDTO) {
+		return Mono.just(LoginMapper.toClient(loginDTO)).flatMap(u -> loginService.getLoginDetails(u.getUsername()))
+				.filter(t -> null != t ? t.getPassword().equals(loginDTO.getPassword()) : null)
+				.switchIfEmpty(Mono.just(new Login())).map(t -> checkStatus(t))
+				.map(r -> new ResponseEntity<>(r, HttpStatus.OK));
 	}
-	
+
 	@PostMapping("/register")
-	public Mono<ResponseEntity<LoginDTO>> create(@RequestBody LoginDTO loginDTO){
-		return Mono.just(LoginMapper.toClient(loginDTO)).flatMap(u->loginService.create(u))
-				.map(LoginMapper::toApi)
-				.map(r-> new ResponseEntity<>(r, HttpStatus.OK));
+	public Mono<ResponseEntity<LoginDTO>> create(@RequestBody LoginDTO loginDTO) {
+		return Mono.just(LoginMapper.toClient(loginDTO)).flatMap(u -> loginService.create(u)).map(LoginMapper::toApi)
+				.map(r -> new ResponseEntity<>(r, HttpStatus.OK));
 	}
-	
+
 	public LoginStatus checkStatus(Login login) {
-		return Optional.ofNullable(login.getUsername()).map(t-> new LoginStatus().setStatus(true))
-		.orElse(new LoginStatus().setStatus(false));
+		return Optional.ofNullable(login.getUsername()).map(t -> new LoginStatus().setStatus(true))
+				.orElse(new LoginStatus().setStatus(false));
 	}
 
 }
